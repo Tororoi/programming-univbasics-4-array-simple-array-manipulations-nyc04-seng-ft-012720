9fdb051bfce00dc043ee9201c6e69d04d28cb8af
Thomas Cantwell <cantwell.tom@gmail.com>
2020-01-09 20:38:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1,array2)
 end
 
 def using_insert(array,element)
-  array[3].insert(element)
+  array.insert(3,element)
 end
 
 def using_uniq(array)
@@ -43,5 +43,5 @@ def using_delete(array,string)
 end
 
 def using_delete_at(array,integer)
-  array[integer].delete
+  array.delete_at(integer)
 end
